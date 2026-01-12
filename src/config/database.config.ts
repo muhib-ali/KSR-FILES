99cff5866b96f7edabfaf067b4c4b3bfc230a16b
arsalan23 <ziaarsalan23@gmail.com>
2026-01-13 00:52:54 +0500

    update DB file for deployement purpose

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -9,7 +9,12 @@ export const dataSourceOptions: DataSourceOptions = {
   database: process.env.DB_NAME || "ksrDb",
   schema: "public",
   synchronize: false,
-  ssl: process.env.DB_SSL === "true",
+  ssl:
+    process.env.DB_SSL === "true"
+      ? {
+        rejectUnauthorized: false,
+      }
+      : false,
   entities: [__dirname + "/../entities/*.entity{.ts,.js}"],
   migrations: ["migrations/*{.ts,.js}"],
 };
@@ -23,7 +28,12 @@ export const appDataSourceOptions: DataSourceOptions = {
   database: process.env.DB_NAME || "ksrDb",
   schema: "public",
   synchronize: false,
-  ssl: process.env.DB_SSL === "true",
+  ssl:
+    process.env.DB_SSL === "true"
+      ? {
+        rejectUnauthorized: false,
+      }
+      : false,
   entities: [__dirname + "/../entities/*.entity{.ts,.js}"],
 };
 
